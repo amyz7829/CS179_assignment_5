0d78a74f5e241f286455ad88ddc999d603b0f348
amyz7829 <amyz7829@gmail.com>
2017-05-09 23:35:33 -0700

    reduced block_size

--- classify_cuda.cu
+++ classify_cuda.cu
@@ -95,8 +95,9 @@ float cudaClassify(
     float *weights,
     cudaStream_t stream)
 {
-    int block_size = (batch_size < 1024) ? batch_size : 1024;
-
+    //int block_size = (batch_size < 1024) ? batch_size : 1024;
+    int block_size = 512;
+    
     // grid_size = CEIL(batch_size / block_size)
     int grid_size = (batch_size + block_size - 1) / block_size;
 
