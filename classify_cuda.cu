755468f88cf621384495a18269a5318e373fa325
amyz7829 <amyz7829@gmail.com>
2017-05-09 23:38:28 -0700

    daata

--- classify_cuda.cu
+++ classify_cuda.cu
@@ -65,7 +65,7 @@ void trainLogRegKernel(
       }
       //Find the gradient for the data point x
       for(int i = 0; i < 50; i++){
-        grad[i] = (1 / batch_size) * (data[idx + 51 * batch_size] * x[i]) / (1 + exp(1 + data[idx + i * batch_size] * error_val));
+        grad[i] = (1 / batch_size) * (data[idx + 51 * batch_size] * data[idx + i * batch_size]) / (1 + exp(1 + data[idx + i * batch_size] * error_val));
       }
 
       //Now atomically build gradient
